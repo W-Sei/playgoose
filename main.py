9b414116a5ff68a22f9e6d16f4e9b0eb26806ce4
Hanna Franko <sei.wrcl@gmail.com>
2023-10-28 15:23:31 +0300

    move down and right

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -19,7 +19,9 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_YELLOWGREEN)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+# player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_right = [1, 0]
 
 playing = True
 
@@ -31,6 +33,14 @@ while playing:
             playing = False
 
     main_display.fill(COLOR_BLACK)
+
+    keys = pygame.key.get_pressed()
+
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
 
     # print(player_rect.bottom)
 
@@ -75,7 +85,7 @@ while playing:
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
+    # player_rect = player_rect.move(player_speed)
 
     pygame.display.flip()
 
